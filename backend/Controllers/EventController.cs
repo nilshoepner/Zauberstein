6a2e3150b517d3f284de05bbe44deac46a9a3f4e
Nils Höpner <hoepner.nils@gmail.com>
2023-06-16 01:08:47 +0200

    wip

---
+++ backend/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EventController : ControllerBase{
+public SqlConnection conn = new SqlConnection();
+
+public EventController(){}
+
+[HttpGet]
+public string Test(){
+// var insert = new SqlCommand("INSERT INTO tbl (xls) VALUES (@xls)", conn);
+// insert.Parameters.AddWithValue("xls", File.ReadAllBytes("template.xls"));
+// insert.ExecuteNonQuery();
+
+    string testString = "";
+    using (SqlConnection connection = new SqlConnection("Server=tcp:zaubersteinserver.database.windows.net,1433;Initial Catalog=zauberstein;Persist Security Info=False;User ID=zauberstein;Password=Tigerberry1432;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+    {
+        connection.Open();
+        testString = connection.ToString();
+        // Do work here; connection closed on following line.
+        connection.Close();
+    }
+    return testString;
+}
+}
